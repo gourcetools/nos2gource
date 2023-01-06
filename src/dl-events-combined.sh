45c8f015a68d20540a7db5c6d609f4cfbf0cde62
gourcetools <120996278+gourcetools@users.noreply.github.com>
2023-01-06 15:05:12 +0100

    fix

--- src/dl-events-combined.sh
+++ src/dl-events-combined.sh
@@ -1,4 +1,4 @@
-LIMIT="10"
+LIMIT="1"
 cd ../gourcelogs
 rm -f *.*
 cd ../src
@@ -7,74 +7,49 @@ cd ../src
 while read RELAY
 do
 	RELAYSHORT=$(echo "$RELAY" | sed 's/^.\{6\}//')
-	echo $RELAY
-	echo $RELAYSHORT
-echo "Downloading events timestamps"
-
-
+	echo "Downloading $LIMIT events from: $RELAYSHORT"
+echo "Downloading events timestamps."
 echo '["REQ", "RAND", {"kinds": [1], "limit": '$LIMIT'}]' |
   nostcat "$RELAY" |
   jq '.[2].created_at' > time
-echo "Downloading events timestamps			done"
-echo "Downloading events timestamps	:)		done"
-echo "Downloading events timestamps			done"
-
-echo "Downloading events pubkeys..."
+echo "Downloading events timestamps								---->	done"
+echo "Downloading events pubkeys."
 echo '["REQ", "RAND", {"kinds": [1], "limit": '$LIMIT'}]' |
   nostcat "$RELAY" |
   jq '.[2].pubkey' > pubkey
-echo "Downloading events pubkeys				done"
-echo "Downloading events pubkeys		:)		done"
-echo "Downloading events pubkeys				done"
-
+echo "Downloading events pubkeys								---->	done"
 # end of 01 dl 
-
-echo "Removing quotes from pubkeys..."
+echo "Removing quotes from pubkeys."
 sed -i 's/^.//' pubkey
+echo "Removing quotes from pubkeys.."
+echo "Removing quotes from pubkeys..."
 sed -i 's/.$//' pubkey
-echo "Removed quotes from pubkeys  		done"
-echo "Removed quotes from pubkeys  	:)	done"
-echo "Removed quotes from pubkeys  		done"
+echo "Removed quotes from pubkeys...  							---->	done"
 cp pubkey file
-
 # end of 02 sed pubkey
-
-echo " Adding |A to pubkeys..."
+echo "Adding |A to pubkeys."
 sed 's/$/|A|/' pubkey > log
-echo " Added |A to pubkeys			done "
-
+echo "Added |A to pubkeys...									---->	done"
 #end of add a
-
+echo "Adding | to time.  "
 sed -i 's/$/|/' time
-echo " Added | to time 						done"
-
+echo "Added | to time											---->	done"
 # end of slash time
-
 echo "Adding relay directory."
 sed -i 's/$/ '$RELAYSHORT'/' log
 sed -i 's/$/\//' log
-
-echo "ok"
-
+echo "Adding relay directory. 							---->	done"
 #end of add relay name
-
-
 paste time log > timelog
 paste timelog file > timelogfile
-
 sed -i 's/$/.event/' timelogfile
-
 # remove tabulation #
 sed -i 's/\t//g' timelogfile
-
 cat timelogfile | sort -n > $RELAYSHORT.txt
 sed -i 's/ //g' $RELAYSHORT.txt
-
-
 mv $RELAYSHORT.txt ../gourcelogs/$RELAYSHORT.txt
-
+echo "Downloading events from: $RELAYSHORT 				---->	done"
 done < relay.txt
-
 rm -f file
 rm -f gourceb
 rm -f log
@@ -86,7 +61,8 @@ rm -f timelogfile
 
 cd ../gourcelogs
 cat *.txt | sort -n > combined.txt
-echo ALL DONE
+awk '!x[substr($0, 1, 6)]++' combined.txt
+echo ALL DONE LETS START GOURCE.
 gource combined.txt
 cd ../src
 echo ALL DONE.
--- src/test.sh
+++
@@ -1,9 +0,0 @@
-while read RELAY
-do
-
-	RELAYSHORT=$(echo "$RELAY" | sed 's/^.\{6\}//')
-
-
-	echo $RELAY
-	echo $RELAYSHORT
-done < relay.txt
